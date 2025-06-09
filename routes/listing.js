406daff2efcc2c83ba3f62ef1fc33ca9e40e44b4
Divy Anant <divyanantx@email.com>
2025-06-09 11:10:16 +0530

    add flash messages

--- routes/listing.js
+++ routes/listing.js
@@ -32,6 +32,10 @@ router.get("/new", (req, res) => {
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("./listings/show.ejs", { listing });
 }));
 
@@ -39,6 +43,10 @@ router.get("/:id", wrapAsync(async (req, res) => {
 router.get("/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+     if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("./listings/edit.ejs", { listing });
 }));
 
